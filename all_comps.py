f73ef64dd24087603c70ea74eca9219e04533d85
NaFreitaz <nunoamaralfreitas@gmail.com>
2019-07-30 15:14:07 +0200

    Newest Version
    newest working version

--- all_comps.py
+++ all_comps.py
@@ -1,34 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[233]:
+# In[238]:
 
 
 import requests
 import datetime
 import pandas as pd
 
-# Today's date
-today = datetime.datetime.now()
 
-# Creating a competition overview dataframe
-competition_overview_df = pd.DataFrame()
-
-# Generate the link for all WCs and get all comp ids
-for season in range(int(str(today.year)[-2:]) + 1):
-    
-    # Generating URLs for world cups since 2000 until this year
-    url = 'http://egw.ifsc-climbing.org/egw/ranking/json.php?type=event&year='
-    url = url + str(today.year - season)
-    season_json = requests.get(url).json()
-    
-    # Getting all information for a dataframe with all comp overall figures
-    competition_overview_df = competition_overview_df.append(add_season_competitions(season_json), ignore_index=True)
-
-competition_overview_df
-
-
-# In[231]:
+# In[239]:
 
 
 # Make a category lookup
@@ -43,7 +24,7 @@ def climbing_category(cat):
     return categories.get(cat, 'otherCategory')
 
 
-# In[223]:
+# In[236]:
 
 
 # Get overall information for all competitions in a season
@@ -72,4 +53,27 @@ def add_season_competitions(url_json):
     
     # Return all new rows for the season
     return new_rows
+
+
+# In[237]:
+
+
+# Today's date
+today = datetime.datetime.now()
+
+# Creating a competition overview dataframe
+competition_overview_df = pd.DataFrame()
+
+# Generate the link for all WCs and get all comp ids
+for season in range(int(str(today.year)[-2:]) + 1):
+    
+    # Generating URLs for world cups since 2000 until this year
+    url = 'http://egw.ifsc-climbing.org/egw/ranking/json.php?type=event&year='
+    url = url + str(today.year - season)
+    season_json = requests.get(url).json()
+    
+    # Getting all information for a dataframe with all comp overall figures
+    competition_overview_df = competition_overview_df.append(add_season_competitions(season_json), ignore_index=True)
+
+competition_overview_df
 
